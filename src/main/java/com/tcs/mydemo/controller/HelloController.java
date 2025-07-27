4a972a72039b01a05f6ef14237529e21e7524f08
Utkarsh <samiam@Utkarshs-MacBook-Air.local>
2025-07-27 11:15:20 -0400

    initial commit

---
+++ src/main/java/com/tcs/mydemo/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.tcs.mydemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+@ResponseBody
+public class HelloController {
+
+
+@RequestMapping("/")
+    public String hello()
+    {
+        return "hello worldsss!";
+    }
+}
