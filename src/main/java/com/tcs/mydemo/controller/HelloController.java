c9d75b56e894226e0fb97cfb44f15715811bb2e8
samiam <grutkarsh@gmail.com>
2025-07-27 23:03:41 -0400

    RequestParam

--- src/main/java/com/tcs/mydemo/controller/HelloController.java
+++ src/main/java/com/tcs/mydemo/controller/HelloController.java
@@ -1,6 +1,7 @@
 package com.tcs.mydemo.controller;
 
 import com.tcs.mydemo.model.User;
+import org.springframework.boot.context.properties.bind.DefaultValue;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
@@ -21,6 +22,14 @@ public class HelloController {
     {
 
         return new User(name,"1","grutkars@gmail.com");
+    }
+
+    @GetMapping("/requestparam")
+    public User requestParam(@RequestParam String name, @RequestParam(required = false, defaultValue = "")  String id)
+    {
+            return new User(name,id,name+"@gmail.com");
+
+
     }
 
 }
