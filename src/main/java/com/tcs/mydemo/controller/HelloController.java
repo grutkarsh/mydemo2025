f4d0d5fb7a1ed4936a73621c7c4e7a732fddc922
samiam <grutkarsh@gmail.com>
2025-07-27 22:45:17 -0400

    Path Variable

--- src/main/java/com/tcs/mydemo/controller/HelloController.java
+++ src/main/java/com/tcs/mydemo/controller/HelloController.java
@@ -2,10 +2,7 @@ package com.tcs.mydemo.controller;
 
 import com.tcs.mydemo.model.User;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.bind.annotation.*;
 
 @Controller
 @ResponseBody
@@ -18,12 +15,12 @@ public class HelloController {
         return "hello worldsss!";
     }
 
-    @GetMapping("/hello")
+    @GetMapping("/hello/{name}")
     //@RequestMapping(value = "/hello",method = RequestMethod.GET)
-    public User hellouser()
+    public User hellouser(@PathVariable  String name)
     {
 
-        return new User("utkarsh","1","grutkars@gmail.com");
+        return new User(name,"1","grutkars@gmail.com");
     }
 
 }
